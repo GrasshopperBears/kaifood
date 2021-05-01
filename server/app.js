d724412144a75eba5122d80c16284a9442e4f385
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-05-01 18:18:52 +0900

    common(chore): dir 설정 - React build directory 설정 - server에서 빌드 파일 serving

--- server/app.js
+++ server/app.js
@@ -9,16 +9,13 @@ import "./models";
 
 const app = express();
 const __dirname = path.resolve();
+const development = process.env.NODE_ENV == "development";
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(logger("dev"));
+app.use(logger(development ? "dev" : "default"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("/", indexRouter);
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,8 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", (req, res, next) => {
+  res.sendFile(path.resolve("dist", "index.html"));
 });
 
 export default router;
