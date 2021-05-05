66243a3464e22895b5c819f180aa561506b6c9c6
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-05-05 22:55:31 +0900

    common(feat): 식당 정보 분리 - url 생성을 위한 식당별 코드 - 식당 정보 json

---
+++ restaurantCodes.js
@@ -0,0 +1,12 @@
+const restaurantCodes = {
+  north: "fclt",
+  west: "west",
+  east: "east1",
+  eastFacaulty: "east2",
+  facaultyHall: "emp",
+  moonji: "icc",
+  hwaAm: "hawam",
+  seoul: "seoul",
+};
+
+export default restaurantCodes;
