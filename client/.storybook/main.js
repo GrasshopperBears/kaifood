972291fd0274cd78fe46e829741a576cbbe2468d
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-05-12 15:26:16 +0900

    client(docs): storybook setting - 불필요한 파일 제거 - alias 추가 - antd style을 preview에 추가

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,11 +1,20 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "../src/components/"),
+      "@layouts": path.resolve(__dirname, "../src/layouts/"),
+      "@pages": path.resolve(__dirname, "../src/pages/"),
+      "@routers": path.resolve(__dirname, "../src/routers/"),
+      "@actions": path.resolve(__dirname, "../src/store/actions/"),
+      "@reducers": path.resolve(__dirname, "../src/store/reducers/"),
+      "@store": path.resolve(__dirname, "../src/store/"),
+      "@services": path.resolve(__dirname, "../src/services/"),
+    };
+    return config;
+  },
+};
