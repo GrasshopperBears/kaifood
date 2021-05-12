b61f2554f994d1d64b1c3c7231a63cf04e3885e8
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-05-12 17:27:15 +0900

    server(fix): user schema 변경 - 유저 이름 필드 추가

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   uid: { type: String, required: true, unique: true },
   nickname: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   userType: {
     type: [{ type: String, enum: ["user", "owner"] }],
   },
