ba6af7200414133085339bd137a78f0eeef953b4
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-05-13 16:06:53 +0900

    server(feat): 중복 유저 관리 - uid가 이미 존재할 경우 error 반환

--- server/services/user/signup.js
+++ server/services/user/signup.js
@@ -6,7 +6,8 @@ const signup = async (req, res, next) => {
     await User.create({ uid, nickname, realname });
     res.json({ success: true });
   } catch (e) {
-    res.json({ success: false, message: "가입 중 오류가 발생했습니다." });
+    if (e.code === 11000) res.json({ success: false, message: "이미 가입된 이메일입니다" });
+    else res.json({ success: false, message: "가입 중 오류가 발생했습니다" });
   }
 };
 
