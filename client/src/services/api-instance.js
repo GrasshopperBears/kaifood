17a440b6434a91318ab8cbe81848e93cd5b3d194
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2021-05-13 20:56:45 +0900

    client(feat): axios instance 변경 - header의 Authorization에 firebase uid 추가

--- client/src/services/api-instance.js
+++ client/src/services/api-instance.js
@@ -1,7 +1,12 @@
 import axios from "axios";
+import firebase from "app-firebase";
 
 const apiInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
+});
+
+apiInstance.interceptors.request.use((config) => {
+  config.headers["Authorization"] = firebase.auth().currentUser.uid ? firebase.auth().currentUser.uid : "";
 });
 
 export default apiInstance;
