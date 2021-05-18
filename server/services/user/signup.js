42826782c9fffcd2c6e1212d6d0939786149790d
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-18 15:39:13 +0900

    server(fix): 회원가입 중 오류 발생 시 error log

--- server/services/user/signup.js
+++ server/services/user/signup.js
@@ -6,6 +6,7 @@ const signup = async (req, res, next) => {
     await User.create({ uid, nickname, realname });
     res.json({ success: true });
   } catch (e) {
+    console.error(e);
     if (e.code === 11000) res.json({ success: false, message: "이미 가입된 이메일입니다" });
     else res.json({ success: false, message: "가입 중 오류가 발생했습니다" });
   }
