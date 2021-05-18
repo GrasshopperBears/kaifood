f93cae78ed46ce52f2dc223cfef6cb205538878a
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-18 21:07:46 +0900

    client(feat): 식당 주인 메인 페이지 구현 - redux 초기화되지 않았을 경우 Spin 표시 - 식당이 없을 경우 없음을 안내하고 식당 추가를 안내 - 식당이 있을 경우 첫 번째 식당 페이지로 이동

--- client/src/pages/owner/MainPage.jsx
+++ client/src/pages/owner/MainPage.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Spin, Empty } from "antd";
 
 const MainPage = () => {
-  return <>owner main</>;
+  const { initialized, restaurants } = useSelector((state) => state.ownerRestaurant);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (initialized && restaurants.length) history.push(`/owner/detail/${restaurants[0]._id}`);
+  }, [initialized]);
+
+  return initialized ? (
+    !restaurants.length && (
+      <Empty
+        description={
+          <>
+            <p>등록된 식당이 없습니다</p>
+            <p>왼쪽 위 메뉴에서 식당을 추가해주세요</p>
+          </>
+        }
+        style={{ marginTop: "50px" }}
+      />
+    )
+  ) : (
+    <Spin />
+  );
 };
 
 export default MainPage;
