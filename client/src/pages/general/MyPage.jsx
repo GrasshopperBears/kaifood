6aed8026080022dc271388faa194d8079575d7f5
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-18 17:04:49 +0900

    client(feat): 주인 되기 버튼 클릭 시 동작 구현 - 성공적으로 식당 주인이 될 경우 userSetOwner reducer를 call - effect 함수를 통해 isOwner가 업데이트 되었을 경우 owner 메인 페이지로 이동

--- client/src/pages/general/MyPage.jsx
+++ client/src/pages/general/MyPage.jsx
@@ -1,15 +1,29 @@
-import React, { useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import MainTitle from "@components/common/MainTitle";
 import CenterDiv from "@components/common/CenterDiv";
+import { userSetOwner } from "@actions/user";
 import requestOwner from "@services/auth/request-owner";
 import { Button, message } from "antd";
 
 const MyPage = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { isOwner } = useSelector((state) => state.userTracker);
+  const { isOwnerOnInit } = useState(isOwner);
+
   const requestOwnerHandler = useCallback(async () => {
     const result = await requestOwner();
     if (!result) return message.error("오류가 발생했습니다");
-    // TODO: 이후 식당 관리 페이지로 이동
-  }, []);
+    dispatch(userSetOwner());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isOwner && !isOwnerOnInit) {
+      history.push("/owner");
+    }
+  }, [isOwner, isOwnerOnInit, history]);
 
   return (
     <>
