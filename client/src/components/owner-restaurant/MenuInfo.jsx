61d4ec61fdd9968616a6711b3c4c959dd1369624
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-19 01:42:03 +0900

    client(feat): 메뉴 정보 컴포넌트 추가

--- client/src/components/owner-restaurant/MenuInfo.jsx
+++ client/src/components/owner-restaurant/MenuInfo.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import AddMenuModal from "./AddMenuModal";
+import { Button } from "antd";
 
 const MenuInfo = () => {
-  return <>MenuInfo</>;
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const showModal = () => {
+    setShowAddModal(true);
+  };
+  const hideModal = () => {
+    setShowAddModal(false);
+  };
+
+  return (
+    <>
+      <Button onClick={showModal} shape="round" style={{ width: "100%", height: "40px" }}>
+        + 메뉴 추가하기
+      </Button>
+      <AddMenuModal visible={showAddModal} hideModal={hideModal} />
+    </>
+  );
 };
 
 export default MenuInfo;
