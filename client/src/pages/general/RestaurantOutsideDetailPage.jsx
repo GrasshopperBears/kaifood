72f648704c9cb341f0e52b3776a929b47dffde4d
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-20 01:36:14 +0900

    client(feat): 외부 식당 상세 페이지 메뉴 표시 - 외부 식당 상세 페이지에 진입 시 식당 정보 및 메뉴를 업데이트 - menus state를 이용해 메뉴 목록을 표시

--- client/src/pages/general/RestaurantOutsideDetailPage.jsx
+++ client/src/pages/general/RestaurantOutsideDetailPage.jsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import MainTitle from "@components/common/MainTitle";
 import RestaurantInfo from "@components/restaurant/RestaurantInfo";
-import { Spin } from "antd";
+import RestaurantOutCampusMenu from "@components/common/RestaurantOutCampusMenu";
+import getRestaurantInfo from "@services/restaurant/get-restaurant-info";
+import getRestaurantMenus from "@services/menu-out-campus/get-restaurant-menus";
+import { Spin, Divider } from "antd";
 import styled from "styled-components";
 
 const RestaurantOutsideDetailPage = () => {
@@ -10,7 +13,16 @@ const RestaurantOutsideDetailPage = () => {
   const [restaurantInfo, setRestaurantInfo] = useState(undefined);
   const [menus, setMenus] = useState([]);
 
-  useEffect(() => {}, [id]);
+  const initRestaurant = async () => {
+    const result = await getRestaurantInfo(id);
+    setRestaurantInfo(result);
+    const menuResult = await getRestaurantMenus(id);
+    setMenus(menuResult);
+  };
+
+  useEffect(() => {
+    initRestaurant();
+  }, [id]);
 
   return restaurantInfo ? (
     <>
@@ -18,6 +30,10 @@ const RestaurantOutsideDetailPage = () => {
       <RestaurantInfoWrapper>
         <RestaurantInfo time={restaurantInfo.time} address={restaurantInfo.address} phoneNumber={restaurantInfo.phoneNumber} />
       </RestaurantInfoWrapper>
+      {menus.length > 0 ? <Divider orientation="left">메뉴 목록</Divider> : <Divider orientation="center">메뉴 없음</Divider>}
+      {menus.map((el) => (
+        <RestaurantOutCampusMenu key={el._id} info={el} />
+      ))}
     </>
   ) : (
     <Spin />
@@ -26,6 +42,9 @@ const RestaurantOutsideDetailPage = () => {
 
 const RestaurantInfoWrapper = styled.div`
   font-size: 0.8rem !important;
+  padding: 10px 20px;
+  border: 1px solid rgba(50, 50, 50, 0.2);
+  border-radius: 6px;
 `;
 
 export default RestaurantOutsideDetailPage;
