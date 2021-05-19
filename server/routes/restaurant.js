f46458777c1b57c5c26bff0dd891b1f672e04689
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-20 00:39:10 +0900

    server(feat): 교외 식당 get api 구현 - 교외 식당 정보를 반환하는 api 구현 - 환경 변수로 approve된 식당만 반환할 것인지 확인

--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -1,6 +1,7 @@
 import express from "express";
 import getGeneralRestaurants from "../services/restaurant/get-general-restaurants";
 import getRestaurantInCampus from "../services/restaurant/get-restaurant-in-campus";
+import getRestaurantOutCampus from "../services/restaurant/get-restaurant-out-campus";
 import addRestaurant from "../services/restaurant/add-restaurant";
 import getRestaurantInfo from "../services/restaurant/get-restaurant-info";
 import editRestaurantInfo from "../services/restaurant/edit-restaurant-info";
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get("/general", getGeneralRestaurants);
 router.get("/in-campus", getRestaurantInCampus);
+router.get("/out-campus", getRestaurantOutCampus);
 router.get("/:rid", getRestaurantInfo);
 router.post("/", identifyUser, addRestaurant);
 router.patch("/:rid", identifyUser, editRestaurantInfo);
