1f8d853dbb001773519d9bda61d3e8afbc267a09
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-20 00:49:53 +0900

    client(feat): RestaurantOutsidePage 초기화 - effect 함수로 서버에서 교외 식당 정보를 가져옴 - 클릭 시 이동할 url 설정

--- client/src/pages/general/RestaurantOutsidePage.jsx
+++ client/src/pages/general/RestaurantOutsidePage.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainTitle from "@components/common/MainTitle";
+import RestaurantCard from "@components/restaurant/RestaurantCard";
+import getRestaurantOutCampus from "@services/restaurant/get-restaurant-out-campus";
 
 const RestaurantOutsidePage = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  const initList = async () => {
+    const result = await getRestaurantOutCampus();
+    setRestaurants(result);
+  };
+
+  useEffect(() => {
+    initList();
+  }, []);
+
   return (
     <>
       <MainTitle>교외 식당</MainTitle>
+      {restaurants.map((el) => (
+        <RestaurantCard key={el._id} info={el} url={`/restaurant/outside/${el._id}`} />
+      ))}
     </>
   );
 };
