ecaddb00a2fad13884adb873e69165899a97abee
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-19 23:41:36 +0900

    client(feat): 식당 관리 메뉴 탭 초기화 설정 - effect 함수를 통해 메뉴 탭이 렌더링될 경우 메뉴를 서버로부터 가져옴 - api 결과로 state 설정 및 화면에 표시

--- client/src/components/owner-restaurant/MenuInfo.jsx
+++ client/src/components/owner-restaurant/MenuInfo.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddMenuModal from "./AddMenuModal";
+import RestaurantOutCampusMenu from "@components/common/RestaurantOutCampusMenu";
+import getRestaurantMenus from "@services/menu-out-campus/get-restaurant-menus";
 import { Button } from "antd";
+import styled from "styled-components";
 
-const MenuInfo = () => {
+const MenuInfo = ({ info: { _id: rid } }) => {
   const [showAddModal, setShowAddModal] = useState(false);
+  const [menus, setMenus] = useState([]);
 
   const showModal = () => {
     setShowAddModal(true);
@@ -12,14 +16,36 @@ const MenuInfo = () => {
     setShowAddModal(false);
   };
 
+  const initMenus = async () => {
+    const result = await getRestaurantMenus(rid);
+    setMenus(result);
+  };
+
+  const addMenuHandler = (newMenu) => {
+    setMenus([...menus, newMenu]);
+  };
+
+  useEffect(() => {
+    initMenus();
+  }, []);
+
   return (
     <>
-      <Button onClick={showModal} shape="round" style={{ width: "100%", height: "40px" }}>
+      <ButtonStyled onClick={showModal} shape="round">
         + 메뉴 추가하기
-      </Button>
-      <AddMenuModal visible={showAddModal} hideModal={hideModal} />
+      </ButtonStyled>
+      {menus.map((el) => (
+        <RestaurantOutCampusMenu key={el._id} info={el} />
+      ))}
+      <AddMenuModal visible={showAddModal} hideModal={hideModal} rid={rid} addState={addMenuHandler} />
     </>
   );
 };
+
+const ButtonStyled = styled(Button)`
+  width: 100%;
+  height: 40px;
+  margin-bottom: 20px;
+`;
 
 export default MenuInfo;
