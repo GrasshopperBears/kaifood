7fa17d2f817912cb0316752941b88d1a33784d0c
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-20 15:07:14 +0900

    client(feat): 예약 페이지 추가 - 유저의 예약 페이지 추가 - 예약 페이지로 이동했을 경우 props로 식당 이름 전달   - 없을 경우 홈페이지로 리디렉트 - DatePicker 추가 - 라우팅 및 페이지 목록에 추가

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,7 @@ import { default as GeneralMainPage } from "./general/MainPage";
 import { default as UserReservationPage } from "./general/ReservationPage";
 import MyPage from "./general/MyPage";
 import RestaurantInsideDetailPage from "./general/RestaurantInsideDetailPage";
+import UserMakeReservationPage from "./general/UserMakeReservationPage";
 import RestaurantGeneralPage from "./general/RestaurantGeneralPage";
 import RestaurantInsidePage from "./general/RestaurantInsidePage";
 import RestaurantOutsidePage from "./general/RestaurantOutsidePage";
@@ -20,6 +21,7 @@ const Pages = {
   UserReservationPage,
   RestaurantGeneralPage,
   RestaurantInsideDetailPage,
+  UserMakeReservationPage,
   RestaurantInsidePage,
   RestaurantOutsidePage,
   AddRestaurantPage,
