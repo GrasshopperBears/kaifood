4f2cb7ced2dedcd80fef7bad3718b8a214fc577f
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-27 23:04:44 +0900

    client(feat): 예약 페이지 초기화 및 예약 가능 여부 확인 - effect 함수로 식당 정보 초기화   - 유효하지 않은 id일 경우 외부 식당 페이지로 이동 - 날짜 및 시간을 선택할 경우 서버에 api를 요청해 예약 가능 여부 확인

--- client/src/pages/general/UserMakeReservationPage.jsx
+++ client/src/pages/general/UserMakeReservationPage.jsx
@@ -1,16 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useLocation, useHistory } from "react-router-dom";
+import moment from "moment";
 import MainTitle from "@components/common/MainTitle";
+import getRestaurantInfo from "@services/restaurant/get-restaurant-info";
+import getRestaurantMenus from "@services/menu-out-campus/get-restaurant-menus";
+import getReservationPossible from "@services/reservation/get-reservation-possible";
 import { Typography, Form, DatePicker } from "antd";
+
+const disabledDate = (current) => current && current < moment().subtract(1, "days").endOf("day");
 
 const UserMakeReservationPage = () => {
   const { id } = useParams();
   const location = useLocation();
   const history = useHistory();
+  const [restaurantInfo, setRestaurantInfo] = useState(location.state.restaurantInfo || undefined);
+  const [menus, setMenus] = useState(location.state.menus || []);
+  const [allowReservation, setAllowReservation] = useState(false);
+
+  const initRestaurant = async () => {
+    try {
+      if (!restaurantInfo) {
+        const result = await getRestaurantInfo(id);
+        if (!result || !result.provideReservation) throw new Error();
+        setRestaurantInfo(result);
+      }
+      if (!menus.length) {
+        const menuResult = await getRestaurantMenus(id);
+        if (!menuResult.length) throw new Error();
+        setMenus(menuResult);
+      }
+    } catch (e) {
+      history.push("/restaurant/out-campus");
+    }
+  };
+
+  const checkReservationValidity = async (time) => {
+    const datetime = moment(time).format("YYYYMMDDTHHmm");
+    const result = await getReservationPossible(id, datetime);
+  };
 
   useEffect(() => {
-    if (!location.state?.name) history.push("/");
-  }, []);
+    initRestaurant();
+  }, [id]);
 
   const submitHandler = async (values) => {};
 
@@ -20,7 +51,7 @@ const UserMakeReservationPage = () => {
       <Typography style={{ marginBottom: "20px" }}>식당 이름: {location.state.name}</Typography>
       <Form name="makeReservation" onFinish={submitHandler}>
         <Form.Item label="예약 날짜">
-          <DatePicker />
+          <DatePicker onChange={checkReservationValidity} disabledDate={disabledDate} format="MM-DD HH:mm" showTime />
         </Form.Item>
       </Form>
     </>
