63c776d16184d9f0dd95d526aae6d622432f7100
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-27 23:02:49 +0900

    server(feat): 예약 가능 여부 확인 API 구현 - 식당 id와 시간 정보를 params로 전달 - 시간과 예약 가능 인원을 기반으로 예약 가능 여부와 해당 시간 최대 예약 가능 인원 반환

--- server/routes/reservation.js
+++ server/routes/reservation.js
@@ -1,8 +1,10 @@
 import express from "express";
 import getAllReservations from "../services/reservation/get-all-reservations";
+import getReservationPossible from "../services/reservation/get-reservation-possible";
 
 const router = express.Router();
 
+router.get("/check/:id/:datetime", getReservationPossible);
 router.get("/all", getAllReservations);
 router.post("/", () => {});
 router.patch("/", () => {});
---
+++ server/services/reservation/get-reservation-possible.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import Restaurant from "../../models/restaurant";
+import Reservation from "../../models/reservation";
+import moment from "moment";
+
+const getReservationPossible = async (req, res) => {
+  const { id, datetime: reqDatetime } = req.params;
+  const rid = mongoose.Types.ObjectId(id);
+  const datetime = moment(reqDatetime);
+  try {
+    const result = await Restaurant.findById(id);
+    const {
+      outCampusTime: { closeDate, startTime, endTime },
+    } = result;
+    const start = moment(startTime);
+    const end = moment(endTime);
+    const startHour = start.hour();
+    const startMinute = start.minutes();
+    const endHour = end.hour();
+    const endMinute = end.minutes();
+    const reqHour = datetime.hour();
+    const reqMinute = datetime.minutes();
+
+    const timeValidity =
+      (reqHour > startHour || (reqHour === startHour && reqMinute >= startMinute)) &&
+      (reqHour < endHour || (reqHour === endHour && reqMinute <= endMinute));
+
+    if (closeDate.includes(datetime.day()) || !timeValidity) return res.json({ possible: false });
+
+    const overlapReservation = await Reservation.find({
+      restaurant: rid,
+      time: { $lte: datetime.subtract(30, "minutes"), $lte: datetime.add(30, "minutes") },
+    });
+    const currentPeople = overlapReservation.reduce((acc, reservation) => {
+      acc += reservation.peopleNumber;
+      return acc;
+    }, 0);
+
+    res.json({ possible: currentPeople < result.maxReservationNumber, currentMaximum: result.maxReservationNumber - currentPeople });
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+};
+
+export default getReservationPossible;
