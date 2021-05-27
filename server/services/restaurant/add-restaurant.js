9293572fe296645384ae014171b2f5176f78109b
Jinwoo Lee <starday456@kaist.ac.kr>
2021-05-27 18:16:59 +0900

    server(feat): 식당 정보에 예약 가능 시간 및 요일 추가 - client로부터 받은 정보 중 휴무일과 예약 가능 시간 저장

--- server/services/restaurant/add-restaurant.js
+++ server/services/restaurant/add-restaurant.js
@@ -1,9 +1,20 @@
 import Restaurants from "../../models/restaurant";
+import moment from "moment";
 
 const addRestaurant = async (req, res) => {
   const uid = req.body.uid;
+  const [startTime, endTime] = req.body.time;
   try {
-    const result = await Restaurants.create({ owner: uid, ...req.body, restaurantType: "restaurant-out" });
+    const result = await Restaurants.create({
+      owner: uid,
+      ...req.body,
+      restaurantType: "restaurant-out",
+      outCampusTime: {
+        closeDate: req.body.closeDate,
+        startTime: moment(startTime),
+        endTime: moment(endTime),
+      },
+    });
     res.json(result);
   } catch (e) {
     console.error(e);
