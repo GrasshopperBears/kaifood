22fd1520be50ad07eaad79a804c1e819131f343d
Jinwoo Lee <starday456@kaist.ac.kr>
2021-06-01 09:52:13 +0900

    client(feat): 식당 주인 예약 내역 탭 구현 - 식당 id가 변경될 경우 예약 내역 조회 - 오늘과 오늘 이후의 예약 내역으로 분리 - 각 list를 OwnerReservationCollection로 전달 - 예약 상태 업데이트 함수 구현 및 props로 전달

--- client/src/components/owner-restaurant/ReservationInfo.jsx
+++ client/src/components/owner-restaurant/ReservationInfo.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import moment from "moment";
+import OwnerReservationCollection from "@components/reservation/OwnerReservationCollection";
+import getOwnerReservations from "@services/reservation/get-owner-reservations";
+import { Empty, Divider } from "antd";
+import { GiSadCrab } from "react-icons/gi";
 
 const ReservationInfo = () => {
-  return <>ReservationInfo</>;
+  const { id } = useParams();
+  const [todayReservations, setTodayReservations] = useState([]);
+  const [otherDayReservations, setOtherDayReservations] = useState([]);
+  // const [oldReservations, setOldReservations] = useState([]);
+
+  const fetchReservations = async () => {
+    const result = await getOwnerReservations(id);
+    setTodayReservations(result.filter((reservation) => moment(reservation.datetime).isSame(moment(), "day")));
+    setOtherDayReservations(result.filter((reservation) => moment(reservation.datetime).isAfter(moment(), "days")));
+    // setOldReservations(result.filter((reservation) => moment().diff(moment(reservation.datetime), "days") > 0));
+  };
+
+  const onAcceptChange = (id, approved, type) => {
+    switch (type) {
+      case 0:
+        setTodayReservations(
+          todayReservations.reduce((acc, reservation) => {
+            if (reservation._id !== id) acc.push(reservation);
+            else acc.push({ ...reservation, approved, pending: false });
+            return acc;
+          }, [])
+        );
+        break;
+      case 1:
+        setOtherDayReservations(
+          otherDayReservations.reduce((acc, reservation) => {
+            if (reservation._id !== id) acc.push(reservation);
+            else acc.push({ ...reservation, approved, pending: false });
+            return acc;
+          }, [])
+        );
+        break;
+      default:
+        return;
+    }
+  };
+
+  useEffect(() => {
+    fetchReservations();
+  }, [id]);
+
+  return (
+    <>
+      {!!todayReservations.length && (
+        <>
+          <OwnerReservationCollection title="오늘" reservations={todayReservations} onAcceptChange={onAcceptChange} type={0} />
+          <Divider style={{ border: "solid 1px #aaaaaa" }} />
+        </>
+      )}
+      {!!otherDayReservations.length && (
+        <OwnerReservationCollection title="이후" reservations={otherDayReservations} onAcceptChange={onAcceptChange} type={1} />
+      )}
+      {otherDayReservations.length + todayReservations.length === 0 && (
+        <Empty image={<GiSadCrab size="80" />} description="예약 내역이 없습니다" style={{ marginTop: "100px" }} />
+      )}
+    </>
+  );
 };
 
 export default ReservationInfo;
