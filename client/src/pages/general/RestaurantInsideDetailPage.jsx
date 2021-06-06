a64964f4c40cb9b08e5bc876d03bff7a76796844
Jinwoo Lee <starday456@kaist.ac.kr>
2021-06-07 02:06:15 +0900

    client(fix): 교내 식당 메뉴 없는 경우 수정 - 메뉴가 없는 경우 Spin이 계속해서 도는 문제 발생 - API로 메뉴를 받아왔는지 여부를 pending으로 확인해서 표시하도록 변경

--- client/src/pages/general/RestaurantInsideDetailPage.jsx
+++ client/src/pages/general/RestaurantInsideDetailPage.jsx
@@ -4,32 +4,39 @@ import MainTitle from "@components/common/MainTitle";
 import RestaurantInfo from "@components/restaurant/RestaurantInfo";
 import RestaurantInsideDaily from "@components/restaurant/RestaurantInsideDaily";
 import getMenuOfRestaurant from "@services/menu-in-campus/get-menu-of-restaurant";
-import { Spin } from "antd";
+import { Spin, Empty } from "antd";
 import styled from "styled-components";
+import { GiSadCrab } from "react-icons/gi";
 
 const RestaurantInsideDetailPage = () => {
   const { code } = useParams();
+  const [pending, setPending] = useState(true);
   const [restaurantInfo, setRestaurantInfo] = useState(undefined);
   const [menus, setMenus] = useState([]);
 
   const initData = useCallback(async () => {
+    setPending(true);
     const result = await getMenuOfRestaurant(code);
-    if (!result.length) return;
-    setRestaurantInfo(result[0].restaurant);
-    setMenus(
-      result.reduce((acc, el) => {
-        const { date, menuList, _id } = el;
-        acc.push({ date, menuList, _id });
-        return acc;
-      }, [])
-    );
+    if (result.length) {
+      setRestaurantInfo(result[0].restaurant);
+      setMenus(
+        result.reduce((acc, el) => {
+          const { date, menuList, _id } = el;
+          acc.push({ date, menuList, _id });
+          return acc;
+        }, [])
+      );
+    }
+    setPending(false);
   }, [code]);
 
   useEffect(() => {
     initData();
-  }, []);
+  }, [initData]);
 
-  return restaurantInfo ? (
+  return pending ? (
+    <Spin />
+  ) : restaurantInfo ? (
     <>
       <MainTitle>{restaurantInfo.name}</MainTitle>
       <RestaurantInfoWrapper>
@@ -40,7 +47,7 @@ const RestaurantInsideDetailPage = () => {
       ))}
     </>
   ) : (
-    <Spin />
+    <Empty image={<GiSadCrab size="80" />} description="메뉴 정보가 없습니다" style={{ marginTop: "100px" }} />
   );
 };
 
